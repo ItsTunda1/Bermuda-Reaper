9ec1123315ea42a3a21729c361bfe2a6728e5f5b
ItsTunda1 <97809650+ItsTunda1@users.noreply.github.com>
2025-10-01 23:35:28 -0400

    works with moving items

--- scripts/radar.gd
+++ scripts/radar.gd
@@ -9,27 +9,19 @@ var radar_points = [Vector2(10, 20), Vector2(-50, 30)]
 
 
 @export var radar_size = Vector2(200, 200)
-@export var radar_range = 1000.0  # maximum distance shown on radar
-
-func _ready():
-	#var dot = RadarDotScene.instantiate()
-	#add_child(dot)
-	#dot.position = Vector2(100, 100)
-	#print("Dot added:", dot)
-	update_radar(radar_points)
+@export var radar_range = 1000.0  # Maximum distance shown on radar
 
 
 func update_radar(points: Array):
-	# Clear existing dots (optional)
+	# Clear existing dots
 	for child in radar_container.get_children():
 		child.queue_free()
 
+	# Add current
 	for point in points:
 		var dot = RadarDotScene.instantiate()
 		radar_container.add_child(dot)
 		dot.position = convert_to_radar_position(point)
-		print("Dot added at:", dot.position)
-		print("Radar container size:", radar_container.size)
 
 func convert_to_radar_position(world_offset: Vector2) -> Vector2:
 	var normalized = world_offset / radar_range  # range -1 to 1
@@ -39,6 +31,7 @@ func convert_to_radar_position(world_offset: Vector2) -> Vector2:
 	
 func _process(_delta):
 	var player_pos = Vector2(0, 0)
+	radar_points[0].x = radar_points[0].x + 10;
 	
 	
 	'var points = []
